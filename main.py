47ff0117319119e0af0e34b0ec402af6ef97085e
Zeke Zumbro <jezumbro@icloud.com>
2025-04-30 11:09:55 -0500

    feat: version3

--- main.py
+++ main.py
@@ -1,13 +1,11 @@
+import math
 from dataclasses import dataclass
 from pathlib import Path
-from typing import Iterable, List, Optional, Tuple
-
-from more_itertools import interleave
+from typing import Iterable, List, Optional
 
 GRID_WIDTH = 42
 GRID_HEIGHT = 7
 SPACING = 1  # mm
-OFFSET_SPACING = 2.95
 NUMBER_OF_ROWS = 2  # x42
 NUMBER_OF_COLUMNS = 2  # x42
 NUMBER_OF_HEIGHT = 3  # x7 would be 21mm
@@ -19,19 +17,30 @@ class Socket:
     diameter: float
     name: Optional[str]
 
+    @property
+    def radius(self) -> float:
+        return self.diameter / 2
+
 
 class SocketGenerator:
     xy_grid = 42
     z_grid = 7
-    _offset_spacing = 2.95
+    _offset_spacing = 2.9
 
-    _cylinder_offset = 1
+    _cylinder_offset = 0.8
 
-    def __init__(self, sockets: List[List[Socket]], rows=1, columns=2, height=3):
+    def __init__(self, sockets: List[List[Socket]]):
         self.sockets: List[List[Socket]] = sockets
-        self.rows = rows
-        self.columns = columns
-        self.units_height = height
+        self.rows = math.ceil(
+            max(s.height for socket in sockets for s in socket) / self.xy_grid
+        )
+        self.columns = math.ceil(
+            sum(s.diameter for socket in sockets for s in socket) / self.xy_grid
+        )
+        self.units_height = (
+            math.ceil(max(s.radius for socket in sockets for s in socket) / self.z_grid)
+            + 1
+        )
 
     @property
     def x_length(self):
@@ -42,8 +51,12 @@ class SocketGenerator:
         return self.xy_grid * self.rows
 
     @property
-    def z_offset(self):
+    def z_length(self):
         return self.z_grid * self.units_height
+
+    @property
+    def max_socket_radius(self):
+        return max(s.radius for sockets in self.sockets for s in sockets)
 
     @property
     def box_cutouts(self) -> str:
@@ -53,31 +66,39 @@ class SocketGenerator:
             + max(s.height for s in self.sockets[0])
             + self._cylinder_offset
         )
+        z_offset = self.z_length - self.max_socket_radius
         box_y = self.y_length - (self._offset_spacing + y_offset)
         full_width = self.x_length - (self._offset_spacing * 2)
-        return f"translate([{x_offset},{y_offset},{self.z_grid}])cube([{full_width:.3f},{box_y:.3f},{(self.z_offset - self.z_grid):.3f}]);"
+        box_z = (self.z_length - z_offset) + 1
+        return f"translate([{x_offset:.3f},{y_offset:.3f},{z_offset:.3f}])cube([{full_width:.3f},{box_y:.3f},{box_z :.3f}]);"
 
     @property
     def header_lines(self) -> Iterable[str]:
         return [
             "include <modules/module_gridfinity.scad>",
-            "union() {",
-            "  difference() {",
-            f"  grid_block({self.columns},{self.rows},{self.units_height});",
+            "$fn=64;",
+            "difference() {",
+            f'grid_block({self.columns},{self.rows},{self.units_height},lip_settings=LipSettings(lipStyle="none"));',
         ]
 
     @property
     def socket_lines(self) -> Iterable[str]:
         for sockets in self.sockets:
             spacing = determine_cylinder_spacing(
-                self.x_length, [s.diameter for s in sockets], self._offset_spacing
+                self.x_length - 2 * self._offset_spacing,
+                [s.diameter for s in sockets],
+                self._offset_spacing,
             )
             for i, socket in enumerate(sockets):
-                radius = socket.diameter / 2
-                trailing = spacing * (1 + i)
-                dx = trailing + sum((s.diameter for s in sockets[:i])) + radius
+                trailing = spacing * i + self._offset_spacing
+                dx = trailing + sum((s.diameter for s in sockets[:i])) + socket.radius
+                yield (
+                    f"translate([{dx:.3f},{socket.height/2 + self._offset_spacing:.3f},{(self.z_length - socket.radius - 0.5):.3f}])"
+                    "rotate([0,0,270])"
+                    f'linear_extrude(5)text("{socket.name}",size=6,halign="center",valign="center");'
+                )
                 yield from self.make_cylinder_lines(
-                    socket, dx, x_offset=self._offset_spacing, z_offset=self.z_offset
+                    socket, dx, x_offset=self._offset_spacing, z_offset=self.z_length
                 )
 
     @property
@@ -85,65 +106,49 @@ class SocketGenerator:
         yield self.box_cutouts
         yield from self.socket_lines
 
-    def make_text_lines(self):
-        for sockets in self.sockets:
-            spacing = determine_cylinder_spacing(
-                self.x_length, [s.diameter for s in sockets], self._offset_spacing
-            )
-            for i, socket in enumerate(sockets):
-                radius = socket.diameter / 2
-                trailing = spacing * (1 + i)
-                dx = trailing + sum((s.diameter for s in sockets[:i])) + radius
-                yield (f'translate([{dx},{socket.height+5},{self.z_grid}])'
-                       'linear_extrude(0.3)'
-                       f'text("{socket.name}",size=3, halign="center");')
-
     def make_cylinder_lines(
         self, socket: Socket, dx, *, x_offset, z_offset
     ) -> Iterable[str]:
-        cylinder_lines = ["rotate([90,0,0])", f"translate([0, {z_offset}, 0])"]
+        cylinder_lines = ["rotate([90,0,0])", f"translate([0, {z_offset:.3f}, 0])"]
         for height, diameter in (
             (socket.height, socket.diameter),
-            (socket.height + 3, socket.diameter - 2),
+            (socket.height + self._cylinder_offset + 1, socket.diameter - 2),
         ):
             yield "".join(
                 [
                     *cylinder_lines,
-                    f"translate([{dx},0,-{height + x_offset}])",
-                    f"cylinder(h={height}, d={diameter});",
+                    f"translate([{dx:.3f},0,-{(height + x_offset):.3f}])",
+                    f"cylinder(h={height:.3f}, d={diameter:.3f});",
                 ]
             )
 
     def write_file(self, path: Path):
         with open(path, "w") as file:
-            file.writelines((f"  {line}\n" for line in self.header_lines))
-            file.writelines(f"    {line}\n" for line in self.generate_lines)
-            file.write("  }\n")
-            file.writelines((f" {line}\n" for line in self.make_text_lines()))
+            file.writelines(
+                f"  {line}\n"
+                for line in (
+                    *self.header_lines,
+                    *self.generate_lines,
+                )
+            )
             file.write("}")
 
 
 def determine_cylinder_spacing(
-    length: float, cylinders: List[float], min_space: float = OFFSET_SPACING
+    length: float, cylinders: List[float], min_space: float
 ) -> float:
     """
     Determines the spacing between the sockets based on their diameters.
     The spacing is calculated as the maximum diameter plus a fixed spacing value.
     """
-    assert len(cylinders), "The list of diameters cannot be empty."
-    num_of_spaces = len(cylinders) + 1
+    assert len(cylinders) > 1, "The list of diameters cannot be empty."
+    num_of_spaces = len(cylinders) - 1
     leftover = length - (sum(cylinders) + min_space * num_of_spaces)
     assert (
         leftover >= 0
     ), f"The sum of diameters exceeds the length. leftover={leftover}"
     delta_leftover = (length - sum(cylinders)) / num_of_spaces
     return delta_leftover
-
-
-def determine_box_spacing(length: float, number_boxes: int) -> float:
-    num_of_spaces = number_boxes + 1
-    usable_space = length - (OFFSET_SPACING * num_of_spaces)
-    return usable_space / number_boxes
 
 
 if __name__ == "__main__":
--- tests/test_main.py
+++ tests/test_main.py
@@ -1,27 +1,28 @@
 import pytest
-from main import determine_cylinder_spacing, write_translations, Socket
+
+from main import Socket, determine_cylinder_spacing
 
 
 @pytest.mark.parametrize(
     "length, diameters",
     [
         (2.0, []),
+        (42, [2]),
         (2.0, [2, 3]),
     ],
 )
 def test_determine_spacing_throws_error(length, diameters):
     with pytest.raises(AssertionError):
-        determine_cylinder_spacing(length, diameters)
+        determine_cylinder_spacing(length, diameters, 1)
 
 
 @pytest.mark.parametrize(
     "length, diameters, expected",
     [
-        (42.0, [2], 20),
-        (42.0, [2, 3, 4, 5, 6, 1], 3),
-        (42.0, [2, 3, 4], 8.25),
+        (42.0, [2, 3, 4, 5, 6, 2], 4),
+        (42.0, [2, 3, 4], 16.5),
     ],
 )
 def test_determine_spacing(length, diameters, expected):
-    output = determine_cylinder_spacing(length, diameters)
+    output = determine_cylinder_spacing(length, diameters, 1)
     assert output == expected, "should return the correct spacing values"
